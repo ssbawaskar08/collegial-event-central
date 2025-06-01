bc496621f34ea38fe44b305de413fd215cf0a5ba
Shreyas Bawaskar <shreyasbawaskar0812@gmail.com>
2025-06-02 00:39:55 +0530

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -10,7 +10,7 @@ const Login = () => {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    console.log("Login attempt:", { rollNo, password });
+    console.log("Login attempt:", rollNo);
     // Add login logic here
   };
 
